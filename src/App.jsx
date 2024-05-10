dcaad68616753caf53a3b59fabe6f287b13f1cb1
younesfdj <ferradjiyounes69@gmail.com>
2024-05-10 10:51:38 +0100

    log

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Angry } from "lucide-react";
 import { UserRound } from "lucide-react";
 export default function App() {
+  log(hello);
   return (
     <div>
       <Angry />
